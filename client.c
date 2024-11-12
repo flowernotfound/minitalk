e3fe1b21b38ba1f057c9bc43195d2169bf6aa35b
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-11-12 15:47:32 +0900

    add draft to client.c

---
+++ client.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int main(int ac, char **av)
+{
+	printf("arg: %s\n", av[1]);
+
+	// 引数を受け取る
+
+	// 渡せる形に変換する
+
+	// シグナル送る
+
+	// ポーズする
+	return 0;
+}
