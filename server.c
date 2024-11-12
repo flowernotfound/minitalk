25250b19588e4897f69841a0fd478d5743250c7b
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-11-13 02:32:57 +0900

    add draft to server.c

---
+++ server.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int main(void)
+{
+	// プロセスのIDを取得
+	// プロセスIDを表示
+
+	//　クライアントからメッセージを受け取る
+	// 	複数のクライアントからメッセージを受け取れるようにする
+	return 0;
+}
