06cccefb5501a5d83e8a773030c7b8c88bb411b2
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-11-13 17:10:50 +0900

    finished receive_signal

--- server.c
+++ server.c
@@ -5,23 +5,24 @@
 
 void receive_signal(int sig)
 {
-	static int count;
+	static int count_bit = 0;
+	static unsigned char current_byte = 0;
 
-	count = 0;
 	// SIGUSR1の場合
 	if (sig == SIGUSR1)
 	{
-
-	}	// SIGUSR2の場合
-	else if (sig ==SIGUSR2)
-	{
+		// 1をセット
+		current_byte |= (1 << (7 - count_bit));
 
 	}
+	count_bit++;
 
 	// 8ビット揃った時には書き出してバッファをリセット
-	if (count == 8)
+	if (count_bit == 8)
 	{
-
+		write(1, &current_byte, 1);
+		count_bit = 0;
+		current_byte = 0;
 	}
 }
 
