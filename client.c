43289fcb5ef8f7e32d4c91c29c6700949a2af78a
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-11-14 01:21:19 +0900

    finished convert_and_send_signal in client.c

--- client.c
+++ client.c
@@ -8,7 +8,25 @@ void convert_and_send_signal(int pid, char c)
 	// 渡せる形に変換する
 	// シグナル送る
 	// 一文字ずつ送信する
-	// シグナルはSIGUSR1とSIGUSR2を使う どっちでもいい
+	// シグナルはSIGUSR1とSIGUSR2を使う SIGUSR1がビット1
+	int bit_position;
+
+	bit_position = 7;
+	while (bit_position >= 0)
+	{
+		if (c & (1 << bit_position))
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				exit(1);
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				exit(1);
+		}
+		usleep(100);
+		bit_position--;
+	}
 
 }
 
@@ -27,14 +45,16 @@ int main(int ac, char **av)
 
 	// サーバーのPID取得
 	pid_server = atoi(av[1]);
+	if (pid_server <= 0)
+		return (1);
 	printf("%d", pid_server);
 
 	i = 0;
 	while (av[2][i])
 	{
 		convert_and_send_signal(pid_server, av[2][i]);
+		i++;
 	}
-
 
 	// ポーズする
 	return 0;
