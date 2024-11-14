c6210f07ff70df5ea5f09b9604eb01ef9e3f02f0
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-11-15 02:27:01 +0900

    remove comments

--- client.c
+++ client.c
@@ -5,10 +5,6 @@
 
 void convert_and_send_signal(int pid, char c)
 {
-	// 渡せる形に変換する
-	// シグナル送る
-	// 一文字ずつ送信する
-	// シグナルはSIGUSR1とSIGUSR2を使う SIGUSR1がビット1
 	int bit_position;
 
 	bit_position = 7;
@@ -27,26 +23,19 @@ void convert_and_send_signal(int pid, char c)
 		usleep(100);
 		bit_position--;
 	}
-
 }
 
 int main(int ac, char **av)
 {
 	pid_t pid_server;
 	int i;
-	// printf("arg1: %s\n", av[1]);
-	// printf("arg2: %s\n", av[2]);
-	// 不正な引数
-	if (ac != 3)
-	{
-		return (1);
-	}
 
-	// サーバーのPID取得
+	if (ac != 3)
+		return (1);
+
 	pid_server = atoi(av[1]);
 	if (pid_server <= 0)
 		return (1);
-	// printf("%d", pid_server);
 
 	i = 0;
 	while (av[2][i])
@@ -55,6 +44,5 @@ int main(int ac, char **av)
 		i++;
 	}
 
-	// ポーズする
 	return 0;
 }
--- server.c
+++ server.c
@@ -8,16 +8,9 @@ void receive_signal(int sig)
 	static int count_bit = 0;
 	static unsigned char current_byte = 0;
 
-	// SIGUSR1の場合
 	if (sig == SIGUSR1)
-	{
-		// 1をセット
 		current_byte |= (1 << (7 - count_bit));
-
-	}
 	count_bit++;
-
-	// 8ビット揃った時には書き出してバッファをリセット
 	if (count_bit == 8)
 	{
 		write(1, &current_byte, 1);
@@ -31,28 +24,16 @@ int main(void)
 	struct sigaction sa;
 	pid_t pid;
 
-	// プロセスのIDを取得
 	pid = getpid();
-
-	// プロセスIDを表示
 	printf("%d\n", pid);
-
-	// シグナル処理の設定
 	sa.sa_handler = receive_signal;
 	sa.sa_flags = 0;
     sigemptyset(&sa.sa_mask);
 	if (sigaction(SIGUSR1, &sa, NULL) == -1)
-	{
 		return (1);
-	}
 	if (sigaction(SIGUSR2, &sa, NULL) == -1)
-	{
 		return (1);
-	}
-
 	while (1)
-	{
 		pause();
-	}
 	return 0;
 }
