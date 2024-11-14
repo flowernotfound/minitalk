d208577a592c4c23129123ec83aeac4f5add6909
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-11-15 01:11:42 +0900

    destroyed alllllllllllllll

--- client_bonus.c
+++ client_bonus.c
@@ -3,76 +3,42 @@
 #include <stdlib.h>
 #include <signal.h>
 
-static int g_received_count = 0;
-
-void received_confirmation(int sig, siginfo_t *info, void *context)
-{
-	(void)info;
-	(void)context;
-
-	if (sig == SIGUSR1)
-		g_received_count++;
-}
-
 void convert_and_send_signal(int pid, char c)
 {
 	int bit_position;
 
 	bit_position = 7;
-
 	while (bit_position >= 0)
 	{
 		if (c & (1 << bit_position))
 		{
 			if (kill(pid, SIGUSR1) == -1)
-			{
-				printf("Failed to send signal\n"); //debug
 				exit(1);
-			}
 		}
 		else
 		{
 			if (kill(pid, SIGUSR2) == -1)
-			{
-		        printf("Failed to send signal\n");//debug
-        	    exit(1);
-			}
+				exit(1);
 		}
-
 		usleep(100);
 		bit_position--;
 	}
-	usleep(100);
+
 }
 
 int main(int ac, char **av)
 {
-	struct sigaction sa;
 	pid_t pid_server;
 	int i;
-	int len;
 
 	if (ac != 3)
+	{
 		return (1);
+	}
 
 	pid_server = atoi(av[1]);
 	if (pid_server <= 0)
 		return (1);
-
-	sa.sa_sigaction = received_confirmation;
-    sa.sa_flags = SA_SIGINFO;
-    sigemptyset(&sa.sa_mask);
-    if (sigaction(SIGUSR1, &sa, NULL) == -1)
-        return (1);
-    if (sigaction(SIGUSR2, &sa, NULL) == -1)
-        return (1);
-
-	len = 0;
-	while (av[2][len])
-		len++;
-	printf("Sent: %d\n", len);
-
-	g_received_count = 0; //これ多分いらない
 
 	i = 0;
 	while (av[2][i])
@@ -80,9 +46,6 @@ int main(int ac, char **av)
 		convert_and_send_signal(pid_server, av[2][i]);
 		i++;
 	}
-
-	usleep(len * 100);
-	printf("Received:  %d\n", g_received_count);
 
 	return 0;
 }
--- server_bonus.c
+++ server_bonus.c
@@ -3,20 +3,10 @@
 #include <stdlib.h>
 #include <signal.h>
 
-void receive_signal(int sig, siginfo_t *info, void *context)
+void receive_signal(int sig)
 {
 	static int count_bit = 0;
 	static unsigned char current_byte = 0;
-	static pid_t client_pid = 0;
-
-	(void)context;
-
-    if (info->si_pid != client_pid) //ここちゃんと書き換えないとクライアントのプロセスIDが更新されなくて複数回の実行で不具合出る
-    { // 今は問題なし
-        client_pid = info->si_pid;
-        count_bit = 0;
-        current_byte = 0;
-    }
 
 	if (sig == SIGUSR1)
 		current_byte |= (1 << (7 - count_bit));
@@ -25,9 +15,6 @@ void receive_signal(int sig, siginfo_t *info, void *context)
 	if (count_bit == 8)
 	{
 		write(1, &current_byte, 1);
-		// 応答
-		if (kill(client_pid, SIGUSR1) == -1)
-            printf("Failed to send confirmation\n"); //debug
 		count_bit = 0;
 		current_byte = 0;
 	}
@@ -40,19 +27,14 @@ int main(void)
 
 	pid = getpid();
 	printf("%d\n", pid);
-
-	sa.sa_sigaction = receive_signal;
-	sa.sa_flags = SA_SIGINFO;
+	sa.sa_handler = receive_signal;
+	sa.sa_flags = 0;
     sigemptyset(&sa.sa_mask);
-
 	if (sigaction(SIGUSR1, &sa, NULL) == -1)
 		return (1);
-
 	if (sigaction(SIGUSR2, &sa, NULL) == -1)
 		return (1);
-
 	while (1)
 		pause();
-
 	return 0;
 }
