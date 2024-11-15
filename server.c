00afdf2aee033ed79f894a10ec11f00798c156fd
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-11-15 14:04:46 +0900

    fixed receive_signal in server.c

--- server.c
+++ server.c
@@ -3,32 +3,43 @@
 #include <stdlib.h>
 #include <signal.h>
 
-void receive_signal(int sig)
+void receive_signal(int sig, siginfo_t *info, void *context)
 {
 	static int count_bit = 0;
+	static pid_t client_pid = 0;
 	static unsigned char current_byte = 0;
 
+	(void)context;
+	if (!client_pid)
+		client_pid = info->si_pid;
 	if (sig == SIGUSR1)
 		current_byte |= (1 << (7 - count_bit));
 	count_bit++;
 	if (count_bit == 8)
 	{
-		write(1, &current_byte, 1);
 		count_bit = 0;
+		if (!current_byte)
+		{
+			kill(client_pid, SIGUSR1);
+			client_pid = 0;
+			return ;
+		}
+		write(1, &current_byte, 1);
 		current_byte = 0;
+		kill(client_pid, SIGUSR2);
 	}
 }
 
 int main(void)
 {
 	struct sigaction sa;
-	pid_t pid;
+	pid_t pid_server;
 
-	pid = getpid();
-	printf("%d\n", pid);
-	sa.sa_handler = receive_signal;
-	sa.sa_flags = 0;
-    sigemptyset(&sa.sa_mask);
+	pid_server = getpid();
+	printf("%d\n", pid_server);
+	sa.sa_sigaction = receive_signal;
+	sa.sa_flags = SA_SIGINFO;
+
 	if (sigaction(SIGUSR1, &sa, NULL) == -1)
 		return (1);
 	if (sigaction(SIGUSR2, &sa, NULL) == -1)
