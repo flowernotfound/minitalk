be14bbc8bceb1f3ce3aedb9a6cb97cdfbc9b3dda
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-11-16 06:31:31 +0900

    gave uuuuuuuuuuup implement again

--- server_bonus.c
+++ server_bonus.c
@@ -1,39 +1,53 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <signal.h>
+#include "minitalk.h"
 
-void receive_signal(int sig)
+void receive_signal(int sig, siginfo_t *info, void *context)
 {
 	static int count_bit = 0;
+	static int pid_client = 0;
 	static unsigned char current_byte = 0;
 
-	if (sig == SIGUSR1)
-		current_byte |= (1 << (7 - count_bit));
+	if (info->si_pid != 0)
+		pid_client = info->si_pid;
+	(void)context;
+
+	if (sig == SIGUSR2)
+		current_byte |= (1 << count_bit);
+
 	count_bit++;
 
 	if (count_bit == 8)
 	{
-		write(1, &current_byte, 1);
 		count_bit = 0;
+		if (current_byte == 0)
+			if (kill(pid_client, SIGUSR2) == -1)
+				error_handler(0);
+		if (current_byte != 0)
+			write(1, &current_byte, 1);
 		current_byte = 0;
 	}
+
+	if (kill(pid_client, SIGUSR1) == -1)
+		error_handler(0);
 }
 
 int main(void)
 {
-	struct sigaction sa;
-	pid_t pid;
+	struct sigaction sa = { 0 };
+	pid_t pid_server;
 
-	pid = getpid();
-	printf("%d\n", pid);
-	sa.sa_handler = receive_signal;
-	sa.sa_flags = 0;
-    sigemptyset(&sa.sa_mask);
-	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+	sa.sa_sigaction = receive_signal;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+    sigaddset(&sa.sa_mask, SIGUSR1);
+    sigaddset(&sa.sa_mask, SIGUSR2);
+	pid_server = getpid();
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		error_handler(1);
 		return (1);
-	if (sigaction(SIGUSR2, &sa, NULL) == -1)
-		return (1);
+	}
+	ft_putnbr(pid_server);
+	write(1, "\n", 1);
 	while (1)
 		pause();
 	return 0;
