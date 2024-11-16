43d1f4d091c816201e5ad6d8f9fc209707129cf9
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-11-16 19:35:41 +0900

    fixed print_pid there was a memory leak...... write 12 -> write 5 in src/server.c

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: hiroaki <hiroaki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 15:18:16 by hiroaki           #+#    #+#             */
-/*   Updated: 2024/11/16 15:33:50 by hiroaki          ###   ########.fr       */
+/*   Updated: 2024/11/16 19:35:09 by hiroaki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ static void	receive_signal(int sig, siginfo_t *siginfo, void *context)
 
 static void	print_pid(pid_t pid_server)
 {
-	write(1, "PID: ", 12);
+	write(1, "PID: ", 5);
 	ft_putnbr(pid_server);
 	write(1, "\n", 1);
 }
